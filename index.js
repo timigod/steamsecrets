bb353813f93532f9f994194f1c120cbba175a7e2
Timi Ajiboye <trypio00@gmail.com>
2024-03-28 13:23:37 +0000

    chore: fixed it all up to work better and seamlessly

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
-var SteamUser = require('steam-user');
+const SteamUser = require('steam-user');
+const readline = require('readline');
+const client = new SteamUser();
 
-var client = new SteamUser();
-
-var logOnOptions = {
-  "accountName": "...",
-  "password": "...",
-  "twoFactorCode": ""
+const logOnOptions = {
+  accountName: '',
+  password: ''
 };
 
-//Login
 client.logOn(logOnOptions);
-client.on('loggedOn', function () {
-  console.log("Logged into Steam");
-  client.enableTwoFactor(function () {
-    for (var i = 0; i < arguments.length; i++) {
-      console.log(arguments[i]);
+
+client.on('loggedOn', () => {
+  console.log('Logged into Steam');
+  client.enableTwoFactor((error, response) => {
+    if (error) {
+      console.error('Error enabling two-factor:', error);
+      return;
     }
+    console.log('Two-factor setup initiated. Please wait...');
+
+    // Logging the secrets for you to save elsewhere
+    if (response.shared_secret) {
+      console.log('Shared Secret:', response.shared_secret);
+      // Ensure to save the shared_secret securely
+    }
+    if (response.identity_secret) {
+      console.log('Identity Secret:', response.identity_secret);
+      // Ensure to save the identity_secret securely
+    }
+
+    // Wait for the SMS code to finalize setup
+    promptForSMSCode(response.shared_secret);
   });
 });
+
+function promptForSMSCode(shared_secret) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question('Please enter the SMS code: ', (smsCode) => {
+    rl.close();
+    client.finalizeTwoFactor(shared_secret, smsCode, (error) => {
+      if (error) {
+        console.error('Error finalizing two-factor setup:', error);
+        return;
+      }
+      console.log('Mobile Authenticator setup complete.');
+    });
+  });
+}
